dea848a7f1d1c3ac6372c9912985bc1e2d68b9e4
prophecy1345 <likeandrewryan@gmail.com>
2026-07-18 14:59:40 +0200

    refactor: replace overly broad CSS selector with data attribute
    Changed `#app div` selector to `[data-habit-card]` to avoid unintended styling of all divs within the app. Added corresponding data attribute to HabitCard component template.

--- frontend/src/components/HabitCard.vue
+++ frontend/src/components/HabitCard.vue
@@ -3,7 +3,7 @@ defineProps<{ id: string; name: string; frequency: string; doneToday: boolean }>
 defineEmits<{ toggle: [id: string] }>();
 </script>
 <template>
-  <div>
+  <div data-habit-card>
     <input type="checkbox" :checked="doneToday" @change="$emit('toggle', id)" />
     <RouterLink :to="`/habits/${id}`">{{ name }}</RouterLink>
   </div>
